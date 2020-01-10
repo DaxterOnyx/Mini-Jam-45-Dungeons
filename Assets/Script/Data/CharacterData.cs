673593a2f5ceebd8de5a6d1697d8028e4a997f5a
Daxter Onyx <daxter5onyx@gmail.com>
2020-01-10 10:43:06 +0100

    first script

---
+++ Assets/Script/Data/CharacterData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New CharacterData", menuName = "Data/CharacterData")]
+public class CharacterData : ScriptableObject
+{
+	public int HealthPoint;
+	public float Range;
+}

42a55f203814bd86a06dc4e7aa0e9bd1502d9995
Labna <alban.cornuau@viacesi.fr>
2020-01-11 20:18:25 +0100

    Table Ascension scrollable - solide

--- Assets/Script/SkillTree/TreeGenerator.cs
+++ Assets/Script/SkillTree/TreeGenerator.cs
@@ -1,37 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TreeGenerator : MonoBehaviour
 {
-    public GameObject TreeTile;
+    public GameObject treeTile;
+    public GameObject container;
     // Start is called before the first frame update
     void Start()
     {
         //newTile.SetActive(false);
         var skillTree = GetComponent<SkillTree>();
         // Create Canvas GameObject.
-        GameObject canvasGO = new GameObject();
-        canvasGO.name = "Canvas";
-        canvasGO.AddComponent<Canvas>();
-        canvasGO.AddComponent<CanvasScaler>();
-        canvasGO.AddComponent<GraphicRaycaster>();
+        //GameObject canvasGO = new GameObject();
+        //canvasGO.name = "Canvas";
+        //canvasGO.AddComponent<Canvas>();
+        //canvasGO.AddComponent<CanvasScaler>();
+        //canvasGO.AddComponent<GraphicRaycaster>();
+        //GameObject viewport = new GameObject();
+        //viewport.name = "viewport";
 
-        // Get canvas from the GameObject.
-        Canvas canvas;
-        canvas = canvasGO.GetComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        Debug.Log(skillTree.Data.skills.Length);
+        ////canvasGO.AddComponent<ScrollRect>();
 
+        //// Get canvas from the GameObject.
+        //Canvas canvas;
+        //canvas = canvasGO.GetComponent<Canvas>();
+        //canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        //var newPanel = Instantiate(TreeTilePanel, new Vector3(Screen.width / 2, Screen.height / 2, 0), Quaternion.identity, canvasGO.transform);
+        var ids = new int[skillTree.Data.skills.Length];
+        for (int i = 0; i < ids.Length; i++)
+        {
+            ids[i] = skillTree.Data.skills[i].id;
+        }
+        Array.Sort(ids);
         // l'arbre est plein (sans interval entre les Id et ID 0 est la racine
         for (int i = 0; i < skillTree.Data.skills.Length; i++)
         {
-            var newTile = Instantiate(TreeTile, new Vector3(
-                180 * (i % 4) + 90, // X
-                Screen.height - (118 * (i / 4) + 59), // Y
-                0), Quaternion.identity , canvasGO.transform).GetComponent<TreeTileScript>();
-            var skill = skillTree.Data.GetSkill(i);
+            var newTile = Instantiate(treeTile, new Vector3(
+                (i % Mathf.FloorToInt(Screen.width / 180)) * 180 - (Mathf.FloorToInt(Screen.width / 180) * 90) + 90 , //180 * (i % (Screen.width / 180)) + 90 , // X
+                0 - (118 * (i / Mathf.FloorToInt(Screen.width / 180))), // Y Screen.height - 59
+                0), Quaternion.identity , container.transform).GetComponent<TreeTileScript>();
+            var skill = skillTree.Data.GetSkill(ids[i]);
             //var dependencies = skill.dependencies;
             //var name = skill.name;                            V
             //var unlocked = skill.unlocked;
@@ -42,8 +54,13 @@ public class TreeGenerator : MonoBehaviour
             newTile.title.text = skill.name;
             newTile.descripion.text = skill.description;
             newTile.cost.text = skill.cost.ToString();
-            newTile.GetComponent<Button>().interactable = skillTree.CanSkillBeUnlocked(i);
+            newTile.GetComponent<Button>().interactable = skillTree.CanSkillBeUnlocked(ids[i]);
         }
+        container.GetComponent<RectTransform>().anchoredPosition = Vector2.zero; //localPosition = Vector3.zero;
+        container.GetComponent<RectTransform>().sizeDelta = new Vector2(
+            0, // X
+            118 + 59 + (118 * (skillTree.Data.skills.Length / Mathf.FloorToInt(Screen.width / 180))) // Y
+            );
         //foreach (var skill in skillTree.Data.skills)
         //{
         //    skill.dependencies

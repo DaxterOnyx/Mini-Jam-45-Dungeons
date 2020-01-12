5ee1b3ce91b67106b2988869c7d8298b4bcedb7a
Labna <alban.cornuau@viacesi.fr>
2020-01-12 16:43:16 +0100

    tree better readability

--- Assets/Scenes/TreeTileScript.cs
+++ Assets/Scenes/TreeTileScript.cs
@@ -5,8 +5,30 @@ using UnityEngine.UI;
 
 public class TreeTileScript : MonoBehaviour
 {
-    public Text title;
-    public Text descripion;
-    public Text cost;
+    [SerializeField]
+    private Text title;
+    [SerializeField]
+    private Text descripion;
+    [SerializeField]
+    private Text cost;
+    private int id;
+    private SkillTree skillTree;
+    [SerializeField]
+    private Button button;
 
+    public void Click()
+    {
+        skillTree.UnlockSkill(id);
+        button.interactable = false;
+    }
+
+    public void initiate(string title, string description, string cost, SkillTree skillTree, int id, bool interactable)
+    {
+        this.skillTree = skillTree;
+        this.title.text = title;
+        this.descripion.text = description;
+        this.cost.text = cost;
+        this.id = id;
+        this.button.interactable = interactable;
+    }
 }
